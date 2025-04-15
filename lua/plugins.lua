fe355bc8d47166e7567ae89b694b30720b842318
Gabriele Frattini <gabbefrattini@gmail.com>
2025-04-15 23:08:04 +0200

    add versions to most plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,60 +1,73 @@
-local status, packer = pcall(require, "packer")
-if (not status) then
-     print("Packer is not installed")
-     return
-end
+return require('packer').startup(function(use)
+  use { 'wbthomason/packer.nvim'}  -- Packer manages itself (no version tag, using latest commit)
 
-vim.cmd [[packadd packer.nvim]]
+  -- Colorscheme (requires colorbuddy)
+  use { 'svrana/neosolarized.nvim', tag = 'v1.1.0',
+    requires = { { 'tjdevries/colorbuddy.nvim', tag = 'v1.0.0' } }
+  }
 
-packer.startup(function(use)
-     use 'wbthomason/packer.nvim'
-     use {
-          'svrana/neosolarized.nvim',
-          requires = { 'tjdevries/colorbuddy.nvim' }
-     }
-     use 'nvim-lualine/lualine.nvim'       -- Statusline
-     use 'nvim-lua/plenary.nvim'           -- Common utilities
-     use 'onsails/lspkind-nvim'            -- vscode-like pictograms
-     use 'hrsh7th/cmp-buffer'              -- nvim-cmp source for buffer words
-     use 'hrsh7th/cmp-nvim-lsp'            -- nvim-cmp source for neovim's built-in LSP
-     use 'hrsh7th/nvim-cmp'                -- Completion
-     use 'neovim/nvim-lspconfig'           -- LSP
-     use 'jose-elias-alvarez/null-ls.nvim' -- Use Neovim as a language server to inject LSP diagnostics, code actions, and more via Lua
-     use 'williamboman/mason.nvim'
-     use 'williamboman/mason-lspconfig.nvim'
-     use 'williamboman/nvim-lsp-installer'
-     use 'glepnir/lspsaga.nvim' -- LSP UIs
-     use 'L3MON4D3/LuaSnip'
-     use {
-          'nvim-treesitter/nvim-treesitter',
-          run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
-     }
-     use 'simrat39/rust-tools.nvim'     -- Rust
-     use 'kyazdani42/nvim-web-devicons' -- File icons
-     use 'nvim-telescope/telescope.nvim'
-     use 'nvim-telescope/telescope-file-browser.nvim'
-     use 'windwp/nvim-autopairs'
-     use 'windwp/nvim-ts-autotag'
-     use 'norcalli/nvim-colorizer.lua'
-     use 'folke/zen-mode.nvim'
-     use({
-          "iamcco/markdown-preview.nvim",
-          run = function() vim.fn["mkdp#util#install"]() end,
-     })
-     use 'akinsho/nvim-bufferline.lua'
-     use 'github/copilot.vim'
+  -- Statusline
+  use { 'nvim-lualine/lualine.nvim', tag = 'compat-nvim-0.6' }
 
-     use 'lewis6991/gitsigns.nvim'
-     use 'dinhhuy258/git.nvim' -- For git blame & browse
-     use 'mbbill/undotree'     -- Undo tree
-     use 'leafOfTree/vim-svelte-theme'
-     use 'nanotee/sqls.nvim'
-     use 'ThePrimeagen/harpoon'
-     -- Java
-     use {
-          "mfussenegger/nvim-jdtls",
-          ft = { "java" },
-     }
-     use("tpope/vim-fugitive")
-     use "lukas-reineke/lsp-format.nvim"
+  -- Common utilities
+  use { 'nvim-lua/plenary.nvim', tag = 'v0.1.4' }
+
+  -- VSCode-like pictograms
+  use { 'onsails/lspkind-nvim'}
+
+  -- CMP and snippet plugins
+  use { 'hrsh7th/cmp-buffer'}
+  use { 'hrsh7th/cmp-nvim-lsp'}
+  use { 'hrsh7th/nvim-cmp'}
+  use { 'L3MON4D3/LuaSnip', tag = 'v2.1.0' }
+
+  -- LSP configurations and tools
+  use { 'neovim/nvim-lspconfig', tag = 'v0.1.7' }
+  use { 'jose-elias-alvarez/null-ls.nvim', tag = 'v2.2.0' }
+  use { 'williamboman/mason.nvim', tag = 'v1.8.3' }
+  use { 'williamboman/mason-lspconfig.nvim', tag = 'v1.26.0' }
+  use { 'williamboman/nvim-lsp-installer'}  -- Last version before deprecation (now replaced by Mason)
+  use { 'glepnir/lspsaga.nvim'}
+  use { 'lukas-reineke/lsp-format.nvim'}
+
+  -- Treesitter
+  use {
+  'nvim-treesitter/nvim-treesitter',
+  commit = '7a2c6211d6f550988fccd8800de8426d5e480a2d',
+  run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
+}
+
+  -- Rust development tools
+  use { 'simrat39/rust-tools.nvim'}
+
+  -- UI enhancements
+  use { 'kyazdani42/nvim-web-devicons'}
+  use { 'akinsho/nvim-bufferline.lua', tag = 'v3.6.2' }
+
+  -- Telescope and extensions
+  use { 'nvim-telescope/telescope.nvim', tag = '0.1.5',
+    requires = { { 'nvim-lua/plenary.nvim' } }
+  }
+  use { 'nvim-telescope/telescope-file-browser.nvim', tag = '*',
+    requires = { 'nvim-telescope/telescope.nvim' }
+  }
+
+  -- Auto pairs and tags
+  use { 'windwp/nvim-autopairs', tag = 'v0.1.6' }
+  use { 'windwp/nvim-ts-autotag'}
+
+  -- Other utilities
+  use { 'norcalli/nvim-colorizer.lua'}
+  use { 'folke/zen-mode.nvim', tag = 'v1.3.0' }
+  use { 'iamcco/markdown-preview.nvim', tag = 'v0.0.10', run = 'cd app && npm install' }
+  use { 'github/copilot.vim'}
+  use { 'lewis6991/gitsigns.nvim', tag = 'v0.7.0' }
+  use { 'dinhhuy258/git.nvim'}
+  use { 'mbbill/undotree'}
+  use { 'leafOfTree/vim-svelte-theme'}
+  use { 'nanotee/sqls.nvim'}
+  use { 'ThePrimeagen/harpoon'}
+  use { 'mfussenegger/nvim-jdtls'}
+  use { 'tpope/vim-fugitive', tag = 'v3.7' }
 end)
+
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/Users/gabrielefrattini/.cache/nvim/packer_hererocks/2.1.1696795921/share/lua/5.1/?.lua;/Users/gabrielefrattini/.cache/nvim/packer_hererocks/2.1.1696795921/share/lua/5.1/?/init.lua;/Users/gabrielefrattini/.cache/nvim/packer_hererocks/2.1.1696795921/lib/luarocks/rocks-5.1/?.lua;/Users/gabrielefrattini/.cache/nvim/packer_hererocks/2.1.1696795921/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/Users/gabrielefrattini/.cache/nvim/packer_hererocks/2.1.1696795921/lib/lua/5.1/?.so"
+local package_path_str = "/Users/gabrielefrattini/.cache/nvim/packer_hererocks/2.1.1703358377/share/lua/5.1/?.lua;/Users/gabrielefrattini/.cache/nvim/packer_hererocks/2.1.1703358377/share/lua/5.1/?/init.lua;/Users/gabrielefrattini/.cache/nvim/packer_hererocks/2.1.1703358377/lib/luarocks/rocks-5.1/?.lua;/Users/gabrielefrattini/.cache/nvim/packer_hererocks/2.1.1703358377/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/Users/gabrielefrattini/.cache/nvim/packer_hererocks/2.1.1703358377/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -180,10 +180,8 @@ _G.packer_plugins = {
     url = "https://github.com/norcalli/nvim-colorizer.lua"
   },
   ["nvim-jdtls"] = {
-    loaded = false,
-    needs_bufread = false,
-    only_cond = false,
-    path = "/Users/gabrielefrattini/.local/share/nvim/site/pack/packer/opt/nvim-jdtls",
+    loaded = true,
+    path = "/Users/gabrielefrattini/.local/share/nvim/site/pack/packer/start/nvim-jdtls",
     url = "https://github.com/mfussenegger/nvim-jdtls"
   },
   ["nvim-lsp-installer"] = {
@@ -264,13 +262,6 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
-vim.cmd [[augroup packer_load_aucmds]]
-vim.cmd [[au!]]
-  -- Filetype lazy-loads
-time([[Defining lazy-load filetype autocommands]], true)
-vim.cmd [[au FileType java ++once lua require("packer.load")({'nvim-jdtls'}, { ft = "java" }, _G.packer_plugins)]]
-time([[Defining lazy-load filetype autocommands]], false)
-vim.cmd("augroup END")
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
